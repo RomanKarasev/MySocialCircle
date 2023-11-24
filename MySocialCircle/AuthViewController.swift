692d2bb42e96d7648fda62a1fe094cad2768f304
RomanKarasev <roman.karasev@icloud.com>
2023-11-24 18:53:01 +0300

    Create SignUpVC and code refactoring

--- MySocialCircle/AuthViewController.swift
+++ MySocialCircle/AuthViewController.swift
@@ -10,63 +10,19 @@ import UIKit
 
 class AuthViewController: UIViewController {
     
-    let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Logo"), contentMode: .scaleAspectFit)
-    
-    let googleLabel = UILabel(text: "Get started with")
-    let emailLabel = UILabel(text: "Or sign up with")
-    let loginLabel = UILabel(text: "Alerady onboard?")
-    
-    let googleButton = UIButton(title: "Google", titleColor: .black, backgroundColor: .white, isShadow: true)
-    let emailButton = UIButton(title: "Email", titleColor: .white, backgroundColor: .buttonDark())
-    let loginButton = UIButton(title: "Login", titleColor: .buttonRed(), backgroundColor: .white, isShadow: true)
-    
+    private let authView = AuthView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view = authView
         view.backgroundColor = .systemBackground
-        setConstraints()
     }
-    
-    private func setConstraints() {
-        
-        let googleView = ButtonLabelView(label: googleLabel, button: googleButton)
-        let emailView = ButtonLabelView(label: emailLabel, button: emailButton)
-        let loginView = ButtonLabelView(label: loginLabel, button: loginButton)
-        
-        let stackView = UIStackView(subviews: [googleView, emailView, loginView],
-                                    axis: .vertical,
-                                    spacing: 40)
-        
-        
-        view.addSubview(logoImageView)
-        view.addSubview(stackView)
-        
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-            ])
-            
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor,
-                                           constant: 160),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor,
-                                               constant: 40),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor,
-                                                constant: -40)
-        ])
-        
-        
-    }
-    
-    
 }
 
 
+
+// MARK: - import SwiftUI
 
 import SwiftUI
 
